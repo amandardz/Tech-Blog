b8b95ca8f706f5cb910f328606e4be4acc7e921a
amandardz <amandardz15@gmail.com>
2021-05-16 16:09:02 -0500

    adding associations

--- models/index.js
+++ models/index.js
@@ -10,4 +10,21 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+})
+
 module.exports = { User, Post }
