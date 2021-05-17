a11f9f29ab6e7c9e2bbad0f10e8303837f5b1b30
amandardz <amandardz15@gmail.com>
2021-05-16 22:11:14 -0500

    making modifications for model/routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -6,13 +6,9 @@ router.post('/', withAuth, async (req, res) => {
   try {
     const newComment = await Comment.create({
       ...req.body,
-      user_id: req.session.user_id,
+      user_id: req.session.user_id
     });
-    
-    console.log(req.body)
-    console.log(newComment)
     res.status(200).json(newComment)
-
   } catch (err) {
     console.log(err)
     res.status(400).json(err);
@@ -33,7 +29,6 @@ router.delete('/:id', withAuth, async (req, res) => {
       return;
     }
     res.status(200).json(commentData);
-    
   } catch (err) {
     res.status(500).json(err);
   }
